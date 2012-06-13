4438fdded99a747d629e5994e56754d5e66c6868
Michele Cattafesta <michelecatta@gmail.com>
2012-06-13 16:15:35 +0200

    First commit
    Signed-off-by: Michele Cattafesta <michelecatta@gmail.com>

---
+++ CircularGauge/CircularGaugeDemoWPF/Window1.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.ComponentModel;
+
+namespace CircularGaugeDemoWPF
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        //Private variables
+        DispatcherTimer timer;
+        Game game1;
+        Game game2;
+        Game game3;
+        Game game4;
+
+        public Window1()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(Window1_Loaded);
+        }
+
+        void Window1_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Set the current value of the gauges
+            game1 = new Game(0);
+            this.myGauge1.DataContext = game1;
+            game2 = new Game(0);
+            this.myGauge2.DataContext = game2;
+            game3 = new Game(0);
+            this.myGauge3.DataContext = game3;
+            game4 = new Game(0);
+            this.myGauge4.DataContext = game4;
+
+            //Start the timer
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(2500);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            //Update random scores
+            Random r = new Random();
+            game1.Score = r.Next(0, 1000);
+            double val = r.Next(1, 10);
+            game2.Score = val / 10;
+            game3.Score = r.Next(-50, 50);
+            game4.Score = r.Next(0, 1000);
+
+
+        }
+
+    }
+
+    /// <summary>
+    /// Helper class to simulate a game
+    /// </summary>
+    public class Game : INotifyPropertyChanged
+    {
+        private double score;
+
+        public double Score
+        {
+            get { return score; }
+            set
+            {
+                score = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Score"));
+                }
+            }
+        }
+
+
+        public Game(double scr)
+        {
+            this.Score = scr;
+        }
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
---
+++ CircularGauge/TestThis/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace TestThis
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private double score = 0;
+        public double Score { get { return score; } set { score = value; this.OnPropertyChanged("Score"); } }
+
+        private double _max;
+        public double Max
+        {
+            get { return _max; }
+            set { _max = value; this.OnPropertyChanged("Max"); }
+        }
+        
+        public MainWindow()
+        {
+            Max = 1000;
+            InitializeComponent();
+            myGauge1.DataContext = this;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Score += 10;
+        }       
+
+        #region INotifyPropertyChanged members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
